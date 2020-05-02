fb34b1083a92aa39537fbd2d7939cc1596a15404
Maspufatullailiyah <maspufa@gmail.com>
2020-05-01 21:08:01 -0700

    Initial commit

---
+++ src/Film.java
@@ -0,0 +1,20 @@
+public class Film {
+        String Genre;
+        int Tahun;
+        String Negara;
+        String Judul;
+        double Rating;
+
+        void cetakInfo(){
+            System.out.println("Genre : " + Genre);
+            System.out.println("Tahun : " + Tahun);
+            System.out.println("Negara : " + Negara);
+            System.out.println("Judul : " + Judul);
+            System.out.println("Rating : " + Rating);
+
+        }
+        void downloadDrama(){
+
+        }
+
+}
